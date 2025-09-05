c245a96672c056ad81616ec3a503824349a9d9e6
angelum23 <angelo.6219@alunosatc.edu.br>
2025-09-05 20:34:40 -0300

    Adiciona multiplica Adiciona testes de unidade para multiplica

--- test_app.py
+++ test_app.py
@@ -1,7 +1,13 @@
-from app import soma
+from app import soma, multiplica
 
 
 def test_soma():
     assert soma(2, 3) == 5
     assert soma(-1, 1) == 0
     assert soma(0, 0) == 0
+
+
+def test_multiplica():
+    assert multiplica(3, 3) == 9
+    assert multiplica(10, 10) == 10
+    assert multiplica(100, 0) == 0
